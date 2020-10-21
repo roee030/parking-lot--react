ec01d09b5663d6b633be763020b52a5bf10556d3
Roee Angel <roee.angel93@gmail.com>
2020-10-21 11:51:30 +0300

    add userdata to local storage

--- src/Components/UserDashBoard.js
+++ src/Components/UserDashBoard.js
@@ -5,16 +5,22 @@ import users from "../utills/users";
 
 export default function UserDashBoard(props) {
   let user = props.history.location.userData;
-  const [arrayOfUsers, setArrayOfUsers] = useState(
-    localStorage.getItem("dataOfUsers")
-  );
+  const [arrayOfUsers, setArrayOfUsers] = useState();
   const [freeParking, setFreeParking] = useState(false);
-  const [dataOfParking, SetDataOfParking] = useState(
-    localStorage.getItem("dataOfParking")
-  );
+  const [dataOfParking, SetDataOfParking] = useState();
+  let [userData, setUserData] = useState(props.history.location.userData);
+  console.log(userData);
+  const getData = () => {
+    setArrayOfUsers(localStorage.getItem("dataOfUsers"));
+    SetDataOfParking(localStorage.getItem("dataOfParking"));
+  };
+  useEffect(() => {
+    console.log("getData");
+    getData();
+    saveUserToLocalStorage(user);
+  });
   const saveUserToLocalStorage = (user) => {
-    if (user) {
-      console.log(user);
+    if (user && arrayOfUsers) {
       // let updateUserObj = JSON.parse(arrayOfUsers).forEach((element) => {
       //   if (element.UserId == user.UserId) {
       //     console.log("duplicate user");
@@ -23,21 +29,55 @@ export default function UserDashBoard(props) {
       let updateUserObj = JSON.parse(arrayOfUsers);
       updateUserObj.push(user);
       console.log(updateUserObj);
+
       localStorage.setItem("dataOfUsers", JSON.stringify(updateUserObj));
+      //   setArrayOfUsers(JSON.stringify(updateUserObj));
     }
   };
+  const constUpdateUserAndSaveToLocalStorage = (user) => {
+    console.log(user);
+    let updateUserArray = JSON.parse(localStorage.getItem("dataOfUsers")).map(
+      (element) => {
+        if (element.userId == user.userId) {
+          return {
+            date: user.date,
+            parkId: user.parkId,
+            parkName: user.parkName,
+            userCarNumber: user.userCarNumber,
+            userCarType: user.userCarType,
+            userId: user.userId,
+          };
+        } else {
+          return element;
+        }
+      }
+    );
+
+    console.log(updateUserArray);
+    userData = updateUserArray;
+  };
   const CheckFreeParking = (user) => {
-    if (user) {
-      const freeParking = JSON.parse(dataOfParking).filter((p) => !p.taken);
+    if (user && dataOfParking) {
+      //   console.log(JSON.parse(dataOfParking));
+      //   console.log(JSON.parse(arrayOfUsers));
+      const freeParking = JSON.parse(
+        localStorage.getItem("dataOfParking")
+      ).filter((p) => !p.taken);
       if (freeParking.length > 0) {
+        console.log("asaaaaaaaaaaaaaaaaaaaaaaaaad");
         user.parkId = freeParking[0].id;
         user.parkName = freeParking[0].name;
+        console.log(user);
+        constUpdateUserAndSaveToLocalStorage(user);
       }
     }
   };
+
   useEffect(() => {
-    saveUserToLocalStorage(user);
-    // const free = CheckFreeParking(user);
+    console.log("effect");
+    // getData();
+
+    const free = CheckFreeParking(user);
   });
 
   return user ? (
@@ -49,7 +89,7 @@ export default function UserDashBoard(props) {
         </div>
         <div className=" data">
           <div>Parking :</div>
-          <div>{user.parkName}</div>
+          <div>{userData.parkName}</div>
         </div>
         <div className=" data">
           <div>Full Name :</div>
